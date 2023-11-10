6c908e201e3c8b53042a9473ba936ead6de0b36a
BR\mob7ca <mob7ca@br.bosch.com>
2023-11-10 16:42:40 -0300

    feat:api

--- restDjango1/cursos/urls.py
+++ restDjango1/cursos/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
 
-from .views import CursoAPIView, AvaliacaoAPIView, AvaliacoesAPIView, CursosAPIView
+from rest_framework.routers import SimpleRouter
+
+from .views import (
+    CursoAPIView, 
+    AvaliacaoAPIView, 
+    AvaliacoesAPIView, 
+    CursosAPIView,
+    CursoViewSet,
+    AvaliacaoViewSet
+    )
+
+router = SimpleRouter()
+router.register('cursos', CursoViewSet)
+router.register('avaliacoes', AvaliacaoViewSet)
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
     path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
+    
     path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
     path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>', AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
    
--- restDjango1/escola/settings.py
+++ restDjango1/escola/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
+Token: 4d25fcb44d7c906cea1f1853aa7d77d00c4f4d52
 """
 
 from pathlib import Path
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     
     'django_filters',
     'rest_framework',
+    'rest_framework.authtoken',
     
     'cursos',
 ]
@@ -135,9 +138,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 2,
+    
 }
